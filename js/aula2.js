1295ea32808dde88dc8b5bc5adcd257c08bf42e7
Rafael Amaral <rafaglhf@gmail.com>
2026-04-06 21:27:08 -0300

    Adicionar verificação para consumo inválido em aula2.js

--- js/aula2.js
+++ js/aula2.js
@@ -1,6 +1,6 @@
 const prompt = require('prompt-sync')();
 
-let consumo = parseFloat(prompt("Digite o consumo em kWh: "));
+let consumo = parseFloat(prompt("Digite o consumo em kWh: ")); isNaN(consumo) ? console.log("Valor inválido. Por favor, digite um número.") : null;
 
 if (consumo <= 100) {
     let valor = consumo * 0.60;
