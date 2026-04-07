5e8554349be4aa06868f0dafcc193f3847a020a0
Rafael Amaral <rafaglhf@gmail.com>
2026-04-06 21:13:11 -0300

    Adicionar arquivos aula1.js e aula2.js com exemplos de variáveis e cálculo de consumo de energia; incluir dependências no package-lock.json e prompt-sync

---
+++ js/aula2.js
@@ -0,0 +1,14 @@
+const prompt = require('prompt-sync')();
+
+let consumo = parseFloat(prompt("Digite o consumo em kWh: "));
+
+if (consumo <= 100) {
+    let valor = consumo * 0.60;
+    console.log("Valor a pagar: R$ " + valor.toFixed(2));
+} else if (consumo <= 300) {
+    let valor = consumo * 0.75;
+    console.log("Valor a pagar: R$ " + valor.toFixed(2));
+} else {
+    let valor = consumo * 0.90;
+    console.log("Valor a pagar: R$ " + valor.toFixed(2));
+}
